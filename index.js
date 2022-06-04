8335c372dc2e386c06cf37efc28f53533066f9e2
Natalia <zakrevskaya.97@inbox.ru>
2022-06-04 19:59:48 +0300

    Added env variables

--- index.js
+++ index.js
@@ -6,56 +6,50 @@ const bodyParser = require('body-parser');
 const app = express()
 
 const corsOptions = {
-    "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204,
-    "allowedHeaders": ["Content-Type"]
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204,
+  "allowedHeaders": ["Content-Type"]
 }
 
 app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-// let smtp_login = process.env.SMTP_LOGIN || 'zakrevskaya.natalia.97@gmail.com'
-// let smtp_password = process.env.SMTP_PASSWORD || 'zak30051997'
-
-let smtp_login = 'zakrevskaya.natalia.97@gmail.com'
-let smtp_password = 'zak30051997'
-
 let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: smtp_login /*'zakrevskaya.natalia.97@gmail.com'*/, // generated ethereal user
-        pass: smtp_password/*'zak30051997'*/, // generated ethereal password
-    },
+  service: 'gmail',
+  auth: {
+    user: process.env.REACT_APP_LOGIN,
+    pass: process.env.REACT_APP_PASSWORD
+  },
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+  res.send('Hello World!')
 })
 app.post('/sendMessage', async function (req, res) {
-    try {
-        let {name, email, phone, message} = req.body;
-        // send mail with defined transport object
-        let info = await transporter.sendMail({
-            from: 'My profile page', // sender address
-            to: 'zakrevskaya.natalia.97@gmail.com', // list of receivers
-            subject: 'testing gmail', // Subject line
-            // text: 'text for testing', // plain text body
-            html: `<b>Message from your portfolio</b> 
+  try {
+    let {name, email, phone, message} = req.body;
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: 'My profile page', // sender address
+      to: process.env.REACT_APP_LOGIN, // list of receivers
+      subject: 'testing gmail', // Subject line
+      // text: 'text for testing', // plain text body
+      html: `<b>Message from your portfolio</b>
 <div>name: ${name}</div>
 <div>email: ${email}</div>
 <div>phone: ${phone}</div>
 <div>message: ${message}</div>`, // html body
-        });
-        res.send('ok!')
-    } catch (e) {
-        console.log('e', e)
-        res.send(e)
-    }
+    });
+    res.send('ok!')
+  } catch (e) {
+    console.log('e', e)
+    res.send(e)
+  }
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
